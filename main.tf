170b30bb92fb798dc40c1d552deb3c76a7246dd0
ggtrd <103002419+ggtrd@users.noreply.github.com>
2025-10-08 15:14:53 +0200

    improve structure

--- main.tf
+++ main.tf
@@ -1,4 +1,34 @@
-locals {
-  main_name = "eks-${var.cluster_name}"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
 }
 
+## PROVIDERS
+provider "aws" {
+  region = "eu-west-1"
+}
+
+
+## VARIABLES
+variable "cluster_name" {
+  description = "The name of the Kubernetes cluster"
+  type        = string
+}
+
+
+## MODULES
+module "cluster" {
+  source = "./modules/module-cluster"
+
+  cluster_name = var.cluster_name
+}
+
+
+# module "storage-state" {
+#   source = "./modules/module-storage-state"
+
+# }
--- modules.tf
+++
@@ -1,9 +0,0 @@
-module "cosmotech-prerequisites" {
-    source = "./modules"
-
-    # EKS
-
-
-    # Terraform state storage
-
-}
